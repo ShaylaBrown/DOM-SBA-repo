2852e120e5c92cb64e6da438ccef069f3e51e268
Shayla Brown <shaylabrown227@gmail.com>
2024-05-01 13:05:34 -0500

    changed the app colors

--- script.js
+++ script.js
@@ -9,3 +9,6 @@ const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
+const taskData = [];
+let currentTask = {};
+openTaskFormBtn.addEventListener("click",() => taskForm.classList.toggle("hidden"));
